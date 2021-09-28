dc28cdd2df28f2e2573e81aa8ba79114d72f1269
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-09-28 13:25:40 +0530

    basic post schema created

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
+const cors = require('cors')
 const connect = require('./configs/db')
 
+const postController = require('./controller/post.controller')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
-
+app.use('/posts',postController)
 app.listen(2345, async ()=>{
     await connect()
     console.log('listening on port 2345')
