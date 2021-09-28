64f4ac9c7ba230a9a092bfc4a8c4dd4e7e84c4e9
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-09-28 11:31:19 +0530

    cloud mongo db connection succesful

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const connect = require('./configs/db')
+
+
+const app = express()
+app.use(express.json())
+
+app.listen(2345, async ()=>{
+    await connect()
+    console.log('listening on port 2345')
+})
