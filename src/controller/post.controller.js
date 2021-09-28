2f650d9ee64c64d34ff53b9e50cc6e99e97da4c2
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-09-28 22:41:35 +0530

    branch merged

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -2,10 +2,13 @@ const router = require('express').Router()
 const Post = require('../model/post.model')
 router.post('/',async (req,res)=>{
     const post = await Post.create(req.body)
+    console.log('post call made')
     res.status(201).json({post})
 })
 router.get('/',async (req,res)=>{
     const posts = await Post.find().lean().exec()
+    
+    console.log('get call made')
     res.status(201).json({posts})
 })
 
