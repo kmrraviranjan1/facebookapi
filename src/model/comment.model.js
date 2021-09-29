45e1d17aa10804e1592f35949368e0606167812f
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-09-29 15:04:00 +0530

    comment controller router done

---
+++ src/model/comment.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Post = require('./post.model')
+const commentSchema = new mongoose.Schema(
+  {
+    post_id: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'post',
+        required:true
+     },
+    body_text: { type: String, required: true },
+    body_photo: { type: String, required: false, default: null },
+    body_video: { type: String, required: false, default: null },
+    
+    emoji: [{ type: String, required: false, default: null }],
+
+    no_of_likes: { type: Number, required: false, default: 0 },
+    liked_by: { type: Number, required: false, default: 0 },
+    no_of_comments: { type: Number, required: false, default: 0 },
+    commentd_by: { type: Number, required: false, default: 0 },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("comment", commentSchema);
