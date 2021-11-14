01bfd3b8056b74d1c2decefc53eb3c3264eaa649
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-11-14 10:02:03 +0530

    config file updated

--- src/configs/db.js
+++ src/configs/db.js
@@ -8,3 +8,10 @@ module.exports = () =>
   }).catch((err)=>{
       console.log(err)
   });
+// mongodb+srv://facebook:facebook@cluster0.lplze.mongodb.net/facebook?retryWrites=true&w=majority
+// PORT=2424
+
+// MONGO_URL=mongodb+srv://akshaykumar:akshay123+@haus.mmor8.mongodb.net/social
+
+// JWT_KEY=facebook
+// mongodb+srv://akshaykumar:akshay123+@haus.mmor8.mongodb.net/social
